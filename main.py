f781b5b83daa69f833f5eea36c550697023273e2
yorlysoro <yorlysoro@gmail.com>
2022-08-14 11:23:11 -0400

    feat: create post tweets

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ class Tweet(BaseModel):
     created_at: datetime = Field(
         default=datetime.now()
     )
-    update_at: Optional[datetime] = Field(
+    updated_at: Optional[datetime] = Field(
         default=None
     )
     by: User = Field(...)
@@ -215,8 +215,35 @@ def home():
     summary="Post a tweet",
     tags=["Tweets"]
 )
-def post():
-    pass
+def post(
+    tweet: Tweet = Body(...)
+):
+    """
+    Post a Tweet
+    This path operation post a tweet in the app
+    Parameters: 
+        - Request body parameter
+            - tweet: Tweet
+    
+    Returns a json with the basic tweet information: 
+        tweet_id: UUID  
+        content: str    
+        created_at: datetime
+        updated_at: Optional[datetime]
+        by: User
+    """
+    with open("tweets.json", "r+", encoding="utf-8") as f:
+        results = json.loads(f.read())
+        tweet_dict = tweet.dict()
+        tweet_dict["tweet_id"] = str(tweet_dict["tweet_id"])
+        tweet_dict["created_at"] = str(tweet_dict["created_at"])
+        tweet_dict["updated_at"] = str(tweet_dict["updated_at"])
+        tweet_dict["by"]["user_id"] = str(tweet_dict["by"]["user_id"])
+        tweet_dict["by"]["birth_date"] = str(tweet_dict["by"]["birth_date"])
+        results.append(tweet_dict)
+        f.seek(0)
+        f.write(json.dumps(results))
+        return tweet
 
 # Show a tweet
 
